d65c21fd727aa65db6811acfc885a21008aa6784
Sadikshya <sadikshyeah@gmail.com>
2026-04-18 22:05:25 +0300

    Refactor AddTraining component to use a single form state and remove unused state variables

--- src/components/AddTraining.tsx
+++ src/components/AddTraining.tsx
@@ -1,9 +1,7 @@
 import { useState } from 'react'
-import dayjs, { type Dayjs } from 'dayjs'
-import { Button, Dialog, DialogActions, DialogContent, DialogTitle, MenuItem, TextField } from '@mui/material'
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
-import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker'
+import { Button, Dialog, DialogActions, DialogTitle } from '@mui/material'
+import TrainingDialogForm from './TrainingDialogForm'
+import { emptyTrainingForm, type TrainingFormValue } from './trainingFormState'
 import type { Customer, NewTraining } from '../types'
 
 type AddTrainingProps = {
@@ -13,12 +11,10 @@ type AddTrainingProps = {
 
 function AddTraining({ customers, handleAdd }: AddTrainingProps) {
   const [open, setOpen] = useState(false)
-  const [date, setDate] = useState<Dayjs | null>(dayjs())
-  const [activity, setActivity] = useState('')
-  const [duration, setDuration] = useState(60)
-  const [customerUrl, setCustomerUrl] = useState('')
+  const [form, setForm] = useState<TrainingFormValue>(() => emptyTrainingForm())
 
   const handleSubmit = () => {
+    const { date, activity, duration, customerUrl } = form
     if (!date || !customerUrl || !activity.trim()) {
       return
     }
@@ -30,10 +26,7 @@ function AddTraining({ customers, handleAdd }: AddTrainingProps) {
       customer: customerUrl,
     })
     setOpen(false)
-    setActivity('')
-    setDuration(60)
-    setCustomerUrl('')
-    setDate(dayjs())
+    setForm(emptyTrainingForm())
   }
 
   return (
@@ -43,41 +36,7 @@ function AddTraining({ customers, handleAdd }: AddTrainingProps) {
       </Button>
       <Dialog open={open} onClose={() => setOpen(false)} fullWidth maxWidth="sm">
         <DialogTitle>New Training</DialogTitle>
-        <DialogContent sx={{ pt: 1, display: 'grid', gap: 2 }}>
-          <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <DateTimePicker
-              label="Date and time"
-              value={date}
-              onChange={(newValue) => setDate(newValue)}
-              slotProps={{ textField: { fullWidth: true } }}
-            />
-          </LocalizationProvider>
-          <TextField label="Activity" value={activity} onChange={(event) => setActivity(event.target.value)} />
-          <TextField
-            label="Duration (min)"
-            type="number"
-            value={duration}
-            onChange={(event) => setDuration(Number(event.target.value))}
-          />
-          <TextField
-            select
-            label="Customer"
-            value={customerUrl}
-            onChange={(event) => setCustomerUrl(event.target.value)}
-          >
-            {customers.map((customer) => {
-              const url = customer._links?.self?.href
-              if (!url) {
-                return null
-              }
-              return (
-                <MenuItem key={url} value={url}>
-                  {`${customer.firstname} ${customer.lastname}`}
-                </MenuItem>
-              )
-            })}
-          </TextField>
-        </DialogContent>
+        <TrainingDialogForm value={form} setValue={setForm} customers={customers} />
         <DialogActions>
           <Button onClick={() => setOpen(false)}>Cancel</Button>
           <Button onClick={handleSubmit}>Save</Button>
